2b18f451f856c6defddacf3e80e4dafdb6cd7d6c
Nicholas Wong <nicholas.wong@redbubble.com>
2016-12-30 16:20:51 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InternApp::Application.config.secret_key_base = 'e7f7091c6896cf1f73d92662574b3fec2cc897b009222e336a38b96afe5799380afe58103ae04ebe5189fdc61b199dd982ec8e1d8a9ce36b5526b489ce24a36d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InternApp::Application.config.session_store :cookie_store, key: '_intern_app_session'
